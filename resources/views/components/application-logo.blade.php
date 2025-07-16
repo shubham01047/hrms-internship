56fa824bb6580e0496e5bdbf60498ff8a97670a6
shubham01047 <shubhamchodankar.26@gmail.com>
2025-07-16 17:28:55 +0530

    changed dashboard and made dynamic logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,8 +1,2 @@
-<svg width="200" height="60" viewBox="0 0 600 180" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="90" cy="90" r="80" stroke="#000" stroke-width="12" fill="none"/>
-  <path d="M60 110 C60 60, 120 60, 120 110" stroke="#000" stroke-width="12" fill="none"/>
-  <circle cx="90" cy="60" r="10" fill="#000"/>
-  <path d="M20 90 L10 70 L40 70 Z" fill="#000"/>
-  <path d="M160 90 L150 110 L180 110 Z" fill="#000"/>
-  <text x="210" y="110" font-family="Arial, sans-serif" font-size="80" fill="#000">HRMS</text>
-</svg>
+<img src="{{ asset($company->logo) }}" alt="HRMS Logo" width="130" height="60" viewBox="0 0 600 180" fill="none">
+
