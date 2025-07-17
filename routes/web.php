453627f7726969c23cf8b2784a99984846f47fdd
shivam <shivambandekar44@gmail.com>
2025-07-17 19:57:03 +0530

    permission module

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HrmsController;
 use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\RolePermissionConrtoller;
+use App\Http\Controllers\PermissionConrtoller;
+use App\Http\Controllers\RoleController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,15 +29,18 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    //Roles and Permissions Route
-    Route::get('/permissions', [RolePermissionConrtoller::class, 'index'])->name('permissions.index');
-    Route::get('/permissions/create', [RolePermissionConrtoller::class, 'create'])->name('permissions.create');
-    Route::post('/permissions', [RolePermissionConrtoller::class, 'store'])->name('permissions.store');
-    Route::get('/permissions/{id}/edit', [RolePermissionConrtoller::class, 'edit'])->name('permissions.edit');
-    Route::post('/permissions/{id}', [RolePermissionConrtoller::class, 'update'])->name('permissions.update');
-    Route::delete('/permissions', [RolePermissionConrtoller::class, 'destroy'])->name('permissions.destroy');
+    //Permissions Route
+    Route::get('/permissions', [PermissionConrtoller::class, 'index'])->name('permissions.index');
+    Route::get('/permissions/create', [PermissionConrtoller::class, 'create'])->name('permissions.create');
+    Route::post('/permissions', [PermissionConrtoller::class, 'store'])->name('permissions.store');
+    Route::get('/permissions/{id}/edit', [PermissionConrtoller::class, 'edit'])->name('permissions.edit');
+    Route::post('/permissions/{id}', [PermissionConrtoller::class, 'update'])->name('permissions.update');
+    Route::delete('/permissions', [PermissionConrtoller::class, 'destroy'])->name('permissions.destroy');
 
-    
+    //Role Route
+    Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
+    Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
+    Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
 });
 
 require __DIR__.'/auth.php';
