8bc23291059358ca2c8b5dc9f25b2b492780548b
shivam <shivambandekar44@gmail.com>
2025-07-17 17:50:13 +0530

    permission module

---
+++ app/Http/Controllers/RolePermissionConrtoller.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionConrtoller extends Controller
+{
+    /**
+     * Display Permissions Page.
+     */
+    public function index()
+    {
+        $permissions = Permission::orderBy('created_at', 'DESC')->paginate(10);
+        return view('permissions.listpermissions', [
+            'permissions' => $permissions
+        ]);
+    }
+
+    /**
+     * Show the form for creating Permission Page.
+     */
+    public function create()
+    {
+        return view('permissions.create');
+    }
+
+    /**
+     * Store a newly created resource in Permission Database.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:permissions|min:4'
+        ]);
+        if ($validator->passes()) {
+            Permission::create(['name' => $request->name]);
+            return redirect()->route('permissions.index')->with('success', 'Permission added sucessfully');
+        } else {
+            return redirect()->route('permissions.create')->withInput()->withErrors($validator);
+        }
+    }
+
+    /**
+     * Show the form for editing the Permission Page.
+     */
+    public function edit($id)
+    {
+        $permission = Permission::findOrFail($id);
+        return view('permissions.edit', [
+            'permission' => $permission
+        ]);
+    }
+
+    /**
+     * Update the specified Permission in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $permission = Permission::findOrFail($id);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:4|unique:permissions,name,' . $id . ',id'
+        ]);
+        if ($validator->passes()) {
+
+            $permission->name = $request->name;
+            $permission->save();
+            return redirect()->route('permissions.index')->with('success', 'Permission updated sucessfully');
+        } else {
+            return redirect()->route('permissions.edit', $id)->withInput()->withErrors($validator);
+        }
+    }
+
+    /**
+     * Deletes the specified Permission from storage.
+     */
+    public function destroy(Request $request)
+    {
+        $id = $request->id;
+        $permission = Permission::findOrFail($id);
+        if ($permission == null) {
+            session()->flash('error', 'Permission not found.');
+            return response()->json([
+                'status' => false
+            ]);
+        }
+        $permission->delete();
+        session()->flash('success', 'Permission deleted successfully.');
+        return response()->json([
+            'status' => true
+        ]);
+    }
+}
---
+++ resources/views/permissions/listpermissions.blade.php
@@ -0,0 +1,71 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-black leading-tight">
+                {{ __('Permissions') }}
+            </h2>
+            <a href="{{ route('permissions.create') }}" class="bg-green-700">Create</a>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-message></x-message>
+            <div class="p-6 text-black">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Created</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($permissions->isNotEmpty())
+                            @foreach ($permissions as $permission)
+                                <tr>
+                                    <td>{{ $permission->id }}</td>
+                                    <td>{{ $permission->name }}</td>
+                                    <td>{{ \Carbon\Carbon::parse($permission->created_at)->format('d M, Y') }}</td>
+                                    <td>
+                                        <a href="{{ route('permissions.edit', $permission->id) }}">Edit</a>
+                                        <a href="javascript:void(0);"
+                                            onclick="deletePermission({{ $permission->id }})">Delete</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+                <div class="my-3">
+                    {{ $permissions->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <x-slot name="script">
+        <script type="text/javascript">
+                // deletePermission function here
+                function deletePermission(id) {
+                    console.log("Calling delete for ID:", id);
+                    if (confirm("Are you sure u want to delete?")) {
+                        $.ajax({
+                            url: '{{ route("permissions.destroy") }}',
+                            type: 'DELETE',
+                            data: {
+                                id: id
+                            },
+                            dataType: 'json',
+                            headers: {
+                                'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                            },
+                            success: function(response) {
+                                window.location.href = '{{ route("permissions.index") }}'
+                            }
+                        });
+                    }
+                }
+        </script>
+    </x-slot>
+</x-app-layout>
