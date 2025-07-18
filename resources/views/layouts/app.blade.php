cf34e04d5020ca11ac76afb14292fa4f7bffbb3e
shubham01047 <shubhamchodankar.26@gmail.com>
2025-07-18 11:23:10 +0530

    roles and permission models

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,30 +17,6 @@
 </head>
 
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen ">
-        @include('layouts.navigation')
-
-        <!-- Page Heading -->
-        @isset($header)
-            <header class="bg-white  shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endisset
-
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
-</body>
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-@isset($script)
-    {{ $script }}
-@endisset
-=======
 <body class="font-sans antialiased bg-gray-100">
     <div class="min-h-screen flex">
         <!-- Sidebar -->
