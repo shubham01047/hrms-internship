1ef8a7c817b47b580d5edffde18689532c98ce79
shubham01047 <shubhamchodankar.26@gmail.com>
2025-07-21 19:49:19 +0530

    improved pagination

--- resources/views/employees/list.blade.php
+++ resources/views/employees/list.blade.php
@@ -5,7 +5,7 @@
                 {{ __('Employees') }}
             </h2>
             @can('create employee')
-                <a href="{{ route('employees.create') }}" class="bg-green-700">Create</a>
+                <a href="{{ route('employees.create') }}" class="success-button">Create</a>
             @endcan
         </div>
     </x-slot>
@@ -16,45 +16,48 @@
             <div class="p-6 text-black">
 
                 <div class="overflow-x-auto">
-    <table class="min-w-full border border-gray-600 rounded-lg shadow-md text-sm text-left text-gray-900">
-        <thead class="bg-red-600 text-white uppercase text-xs tracking-wider">
-            <tr class="border-b border-gray-800">
-                <th class="px-6 py-3">#</th>
-                <th class="px-6 py-3">Full Name</th>
-                <th class="px-6 py-3">Email</th>
-                <th class="px-6 py-3">Created</th>
-                <th class="px-6 py-3">Action</th>
-            </tr>
-        </thead>
-        <tbody class="divide-y divide-gray-300 bg-white">
-            @if ($employees->isNotEmpty())
-                @foreach ($employees as $index => $employee)
-                    <tr class="hover:bg-gray-100 transition">
-                        <td class="px-6 py-4">{{ $index + 1 }}</td>
-                        <td class="px-6 py-4">{{ $employee->first_name . ' ' . $employee->last_name }}</td>
-                        <td class="px-6 py-4">{{ $employee->email }}</td>
-                        <td class="px-6 py-4">{{ \Carbon\Carbon::parse($employee->created_at)->format('d M, Y') }}</td>
-                        <td class="px-6 py-4 space-x-2">
-                            @can('edit employee')
-                                <a href="{{ route('employees.edit', $employee->id) }}"
-                                   class="text-blue-600 hover:underline font-medium">Edit</a>
-                            @endcan
-                            @can('delete employee')
-                                <a href="javascript:void(0);"
-                                   onclick="deleteEmployee({{ $employee->id }})"
-                                   class="text-red-600 hover:underline font-medium">Delete</a>
-                            @endcan
-                        </td>
-                    </tr>
-                @endforeach
-            @else
-                <tr>
-                    <td colspan="5" class="px-6 py-4 text-center text-gray-500">No employees found.</td>
-                </tr>
-            @endif
-        </tbody>
-    </table>
-</div>
+                    <table
+                        class="min-w-full border border-gray-600 rounded-lg shadow-md text-sm text-left text-gray-900">
+                        <thead class="bg-red-600 text-white uppercase text-xs tracking-wider">
+                            <tr class="border-b border-gray-800">
+                                <th class="px-6 py-3">#</th>
+                                <th class="px-6 py-3">Full Name</th>
+                                <th class="px-6 py-3">Email</th>
+                                <th class="px-6 py-3">Created</th>
+                                <th class="px-6 py-3">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-300 bg-white">
+                            @if ($employees->isNotEmpty())
+                                @foreach ($employees as $index => $employee)
+                                    <tr class="hover:bg-gray-100 transition">
+                                        <td class="px-6 py-4">{{ $index + 1 }}</td>
+                                        <td class="px-6 py-4">{{ $employee->first_name . ' ' . $employee->last_name }}
+                                        </td>
+                                        <td class="px-6 py-4">{{ $employee->email }}</td>
+                                        <td class="px-6 py-4">
+                                            {{ \Carbon\Carbon::parse($employee->created_at)->format('d M, Y') }}</td>
+                                        <td class="flex space-x-2 px-6 py-4 space-x-2">
+                                            @can('edit employee')
+                                                <a href="{{ route('employees.edit', $employee->id) }}"
+                                                    class="inline  px-3 py-1 text-xs font-medium text-white bg-blue-600 rounded hover:bg-blue-700 transition"><x-pencil/>Edit</a>
+                                            @endcan
+                                            @can('delete employee')
+                                                <a href="javascript:void(0);" onclick="deleteEmployee({{ $employee->id }})"
+                                                    class="inline-block px-3 py-1 text-xs font-medium text-white bg-red-600 rounded hover:bg-red-700 transition "><x-trashcan/>Delete</a>
+                                            @endcan
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td colspan="5" class="px-6 py-4 text-center text-gray-500">No employees found.
+                                    </td>
+                                </tr>
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
 
                 <div class="my-3">
                     {{ $employees->links() }}
